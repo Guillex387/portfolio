37a9f3fac3c0734a7eb2f84bae6577539366c043
Guillex387 <guillesiglez387@gmail.com>
2021-12-13 22:43:24 +0000

    Other config The another doesn't work

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,9 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
+export function cors(res: VercelResponse) {
+  return res.setHeader('Access-Control-Allow-Origin', '*');
+}
+
 export default (req: VercelRequest, res: VercelResponse) => {
-  res.send('Welcome to the Guillex387 portfolio API 👨‍💻');
+  cors(res).send('Welcome to the Guillex387 portfolio API 👨‍💻');
 };
