7c3627426d2e4b3dcabf381ff683aece34392813
Guillex387 <guillesiglez387@gmail.com>
2021-12-21 03:53:19 +0000

    I added the cache system

--- api/program/[name]/index.ts
+++ api/program/[name]/index.ts
@@ -1,4 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import cache, { updateCache } from '../../../lib/cache';
 import NotionAPI from '../../../lib/db';
 import GithubAPI from '../../../lib/github';
 import { cors } from '../../index';
@@ -6,25 +7,19 @@ import { cors } from '../../index';
 const githubAPI = new GithubAPI();
 const notionAPI = new NotionAPI();
 
-export default async (req: VercelRequest, res: VercelResponse) => {
-  let programName = req.query.name.toString();
-  let program = await notionAPI.getProgram(programName);
-  if (!program)
-    return cors(res)
-      .status(404)
-      .send('Program not found');
-  if (program['repo-name']) {
-    let repo = await githubAPI.getRepo(program['repo-name'], program.owner);
-    if (!repo)
-      return cors(res)
-        .status(404)
-        .send('Github repository not found');
-    let readme = await githubAPI.getReadme(program['repo-name'], program.owner);
-    if (!readme)
-      return cors(res)
-        .status(404)
-        .send('Readme of repository not found');
-    return cors(res).json({ ...program, ...repo, readme });
-  }
-  return cors(res).json(program);
-};
+export default async (req: VercelRequest, res: VercelResponse) =>
+  await cache(req, cors(res), async (req, res) => {
+    let programName = req.query.name.toString();
+    let program = await notionAPI.getProgram(programName);
+    if (!program) return res.status(404).send('Program not found');
+    if (program['repo-name']) {
+      let repo = await githubAPI.getRepo(program['repo-name'], program.owner);
+      if (!repo) return res.status(404).send('Github repository not found');
+      let readme = await githubAPI.getReadme(program['repo-name'], program.owner);
+      if (!readme) return res.status(404).send('Readme of repository not found');
+      await updateCache(req.url || 'null-route', { ...program, ...repo, readme });
+      return res.json({ ...program, ...repo, readme });
+    }
+    await updateCache(req.url || 'null-route', program);
+    return res.json(program);
+  });
--- api/program/[name]/releases/index.ts
+++ api/program/[name]/releases/index.ts
@@ -1,4 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import cache, { updateCache } from '../../../../lib/cache';
 import NotionAPI from '../../../../lib/db';
 import GithubAPI from '../../../../lib/github';
 import { cors } from '../../../index';
@@ -6,22 +7,16 @@ import { cors } from '../../../index';
 const githubAPI = new GithubAPI();
 const notionAPI = new NotionAPI();
 
-export default async (req: VercelRequest, res: VercelResponse) => {
-  let programName = req.query.name.toString();
-  let program = await notionAPI.getProgram(programName);
-  if (!program)
-    return cors(res)
-      .status(404)
-      .send('Program not found');
-  if (program['repo-name']) {
-    let releases = await githubAPI.getReleases(program['repo-name'], program.owner);
-    if (!releases)
-      return cors(res)
-        .status(404)
-        .send('Github releases not found');
-    return cors(res).json(releases);
-  }
-  return cors(res)
-    .status(400)
-    .send('The program not have releases');
-};
+export default async (req: VercelRequest, res: VercelResponse) =>
+  await cache(req, cors(res), async (req, res) => {
+    let programName = req.query.name.toString();
+    let program = await notionAPI.getProgram(programName);
+    if (!program) return res.status(404).send('Program not found');
+    if (program['repo-name']) {
+      let releases = await githubAPI.getReleases(program['repo-name'], program.owner);
+      if (!releases) return res.status(404).send('Github releases not found');
+      await updateCache(req.url || 'null-route', releases);
+      return res.json(releases);
+    }
+    return res.status(400).send('The program not have releases');
+  });
